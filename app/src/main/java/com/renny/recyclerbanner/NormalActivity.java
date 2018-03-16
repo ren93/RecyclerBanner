6181d6c46eddffcdf926896132a9f4bd4b4189ce
Renny <renjianan@aicaigroup.com>
2018-03-16 16:35:24 +0800

    feat:整体结构更改

--- app/src/main/java/com/renny/recyclerbanner/NormalActivity.java
+++ app/src/main/java/com/renny/recyclerbanner/NormalActivity.java
@@ -20,10 +20,12 @@ public class NormalActivity extends AppCompatActivity {
         banner = (RecyclerViewBannerNormal) findViewById(R.id.banner);
         banner2 = (RecyclerViewBannerNormal) findViewById(R.id.banner2);
         List<String> list = new ArrayList<>();
-        list.add("http://oo6pz0u05.bkt.clouddn.com/17-12-13/69427561.jpg");
-        list.add("http://oo6pz0u05.bkt.clouddn.com/17-12-13/23738150.jpg");
-        list.add("http://oo6pz0u05.bkt.clouddn.com/17-12-13/30127126.jpg");
-        list.add("http://oo6pz0u05.bkt.clouddn.com/17-12-13/36125492.jpg");
+        list.add("http://img0.imgtn.bdimg.com/it/u=1352823040,1166166164&fm=27&gp=0.jpg");
+        list.add("http://img3.imgtn.bdimg.com/it/u=2293177440,3125900197&fm=27&gp=0.jpg");
+        list.add("http://img3.imgtn.bdimg.com/it/u=3967183915,4078698000&fm=27&gp=0.jpg");
+        list.add("http://img0.imgtn.bdimg.com/it/u=3184221534,2238244948&fm=27&gp=0.jpg");
+        list.add("http://img4.imgtn.bdimg.com/it/u=1794621527,1964098559&fm=27&gp=0.jpg");
+        list.add("http://img4.imgtn.bdimg.com/it/u=1243617734,335916716&fm=27&gp=0.jpg");
         banner.initBannerImageView(list, new RecyclerViewBannerBase.OnBannerItemClickListener() {
             @Override
             public void onItemClick(int position) {
