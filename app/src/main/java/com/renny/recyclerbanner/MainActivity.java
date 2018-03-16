db6449995a4c2a6bebf779f75cca69b9a803114c
Renny <renjianan@aicaigroup.com>
2018-03-16 18:23:14 +0800

    feat:滚动效果

--- app/src/main/java/com/renny/recyclerbanner/MainActivity.java
+++ app/src/main/java/com/renny/recyclerbanner/MainActivity.java
@@ -25,9 +25,13 @@ public class MainActivity extends AppCompatActivity implements BannerLayout.OnBa
 
         List<String> list = new ArrayList<>();
         list.add("http://img0.imgtn.bdimg.com/it/u=1352823040,1166166164&fm=27&gp=0.jpg");
-
-        WebBannerAdapter webBannerAdapter=new WebBannerAdapter(this,list);
-        webBannerAdapter.setOnBannerItemClickListener(new BannerLayout.OnBannerItemClickListener() {
+        list.add("http://img3.imgtn.bdimg.com/it/u=2293177440,3125900197&fm=27&gp=0.jpg");
+        list.add("http://img3.imgtn.bdimg.com/it/u=3967183915,4078698000&fm=27&gp=0.jpg");
+        list.add("http://img0.imgtn.bdimg.com/it/u=3184221534,2238244948&fm=27&gp=0.jpg");
+        list.add("http://img4.imgtn.bdimg.com/it/u=1794621527,1964098559&fm=27&gp=0.jpg");
+        list.add("http://img4.imgtn.bdimg.com/it/u=1243617734,335916716&fm=27&gp=0.jpg");
+        WebBannerAdapter mzBannerAdapter=new WebBannerAdapter(this,list);
+        mzBannerAdapter.setOnBannerItemClickListener(new BannerLayout.OnBannerItemClickListener() {
             @Override
             public void onItemClick(int position) {
                 Toast.makeText(MainActivity.this, "点击了第  " + position+"  项", Toast.LENGTH_SHORT).show();
@@ -41,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements BannerLayout.OnBa
                 Toast.makeText(MainActivity.this, "点击了第  " + position+"  项", Toast.LENGTH_SHORT).show();
             }
         });
-        recyclerBanner.setAdapter(webBannerAdapter);
+        recyclerBanner.setAdapter(mzBannerAdapter);
         bannerVertical.setAdapter(mzBannerAdapter2);
     }
 
